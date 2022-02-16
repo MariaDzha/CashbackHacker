b31b7f5987738c7dfd0205fb76d4602778e1c431
MariaDzha <m_zudina@inbox.ru>
2022-02-16 22:53:46 +0300

    Tests done

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,44 +1,43 @@
 package ru.netology.unit;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void AmountUnderBoundary (){
         int amount = 900;
         int expected = 100;
         int actual = cashbackHackService.remain(900);
-        assertEquals (actual, expected);
+        assertEquals (expected, actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void AmountOverBoundary (){
         int amount = 1500;
         int expected = 500;
         int actual = cashbackHackService.remain(1500);
-        assertEquals (actual, expected);
+        assertEquals (expected, actual);
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void AmountEqualToBoundary (){
         int amount = 1000;
         int expected = 0;
         int actual = cashbackHackService.remain(1000);
-        assertEquals (actual, expected);
+        assertEquals (expected, actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void AmountIsZero (){
         int amount = 0;
         int expected = 1000;
         int actual = cashbackHackService.remain(0);
-        assertEquals (actual, expected);
+        assertEquals (expected, actual);
 
     }
 
